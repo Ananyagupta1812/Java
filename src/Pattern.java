9f7b1ed1862256e49684c7287d047badb5a4e364
Ananya Gupta <121352621+Ananyagupta1812@users.noreply.github.com>
2023-02-07 21:43:33 +0530

    Update Pattern.java

--- src/Pattern.java
+++ src/Pattern.java
@@ -22,3 +22,21 @@ public class Main {
         }
     }
 }
+// Code to print hollow rectangle pattern
+public class Main {
+    public static void main(String[] args) {
+        int n=4;
+        int m=5;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(i=1 || j=1 || i=n || j=m){
+                    System.out.println("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("\n");
+        }
+    }
+}
