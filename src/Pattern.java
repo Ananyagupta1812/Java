64a6c45d8dc684d70e56558eccc6c71191a408f1
Ananya Gupta <121352621+Ananyagupta1812@users.noreply.github.com>
2023-02-07 21:03:15 +0530

    Create Pattern.java

---
+++ src/Pattern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+//Code to print star pattern.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
+
+// Code to print solid rectangle pattern
+public class Main {
+    public static void main(String[] args) {
+        int n=4;
+        int m=5;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                System.out.println("*");
+            }
+            System.out.println("\n");
+        }
+    }
+}
